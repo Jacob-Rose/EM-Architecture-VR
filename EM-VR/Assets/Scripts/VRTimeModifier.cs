06b93b01a9ff61ae4924ba0e05f4ef0e29752981
Jake Rose <jacob.rose@mymail.champlain.edu>
2019-11-26 17:22:08 -0500

    small tweaks, ready to add custom controls for vr controllers

---
+++ EM-VR/Assets/Scripts/VRTimeModifier.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Valve.VR.InteractionSystem;
+
+public class VRTimeModifier : MonoBehaviour
+{
+    public Hand lftHand;
+    public Hand rgtHand;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
