319325ee54dad2a540bb4589aaee1049cb20fe0c
Jake Rose <jacob.rose@mymail.champlain.edu>
2019-12-12 15:38:04 -0500

    small changes for vr controls

--- EM-VR/Assets/Scripts/VRTimeModifier.cs
+++ EM-VR/Assets/Scripts/VRTimeModifier.cs
@@ -1,21 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-
+using Valve.VR;
 using Valve.VR.InteractionSystem;
 
 public class VRTimeModifier : MonoBehaviour
 {
-    public Hand lftHand;
-    public Hand rgtHand;
-    void Start()
-    {
-        
-    }
+    public TextMesh dateTextMesh;
+    public SteamVR_Action_Vector2 m_TouchPosition;
+    public Hand m_LftHand;
+    public Hand m_RgtHand;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        m_TouchPosition = new SteamVR_Action_Vector2();
+    }
     void Update()
     {
-        
+        float newSpeed = m_TouchPosition[SteamVR_Input_Sources.LeftHand].delta.x;
+        TimelineController.Instance.timeLineSpeed += newSpeed;
+
+        dateTextMesh.text = TimelineController.Instance.currentTimelineDate.day.ToString() + " " 
+            + TimelineController.Instance.currentTimelineDate.month.ToString() + " " 
+            + TimelineController.Instance.currentTimelineDate.year.ToString() + " "
+            + TimelineController.Instance.timeLineSpeed.ToString();
     }
 }
